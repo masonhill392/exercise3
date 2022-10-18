edcff1ba41e210d1dd206947657d0f7c68b2ee95
masonhill392 <masonhill392@gmail.com>
2022-10-18 15:34:53 -0600

    10-18 Final

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,24 @@
 var app = new Framework7({
-    // App root element
-    el: '#app',
-    // ... other parameters
-  });
+  // App root element
+  el: '#app',
+  routes: [
+    {
+      path: '/',
+      url: 'index.html',
+    },
+    {
+      path: '/podcasts/',
+      url: 'podcasts.html',
+    },
+    {
+      path: '/movies/',
+      url: 'movies.html',
+    },
+    {
+      path: '/books/',
+      url: 'books.html',
+    },
+  ],
+  // ... other parameters
+});
 var mainView = app.views.create('.view-main')
